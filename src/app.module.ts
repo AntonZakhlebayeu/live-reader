18f995025f9865cb008f81ae2888ed28ca6387a8
Anton <antonzakhlebayeu@gmail.com>
2022-08-05 00:31:13 +0300

    Set up the docker and docker-compose. Set up redis for caching.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
