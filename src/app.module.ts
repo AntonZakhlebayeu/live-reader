b028f8157d71fa657dbbd7dd20da244bf4c383f3
Anton <antonzakhlebayeu@gmail.com>
2022-08-05 00:53:07 +0300

    Set up typeorm and database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,6 +11,16 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
