14178a9740b266649c8f9d46b5d876cbea93e47b
Anton <antonzakhlebayeu@gmail.com>
2022-08-05 00:08:41 +0300

    Added and set up swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageJson from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Live-Reader')
+    .setDescription('The live-reader API description')
+    .setVersion(packageJson.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  await app.listen(8000);
 }
 bootstrap();
