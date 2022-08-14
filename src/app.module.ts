8ab096af63cfbb1ffd03bf24ad841dd363a8a7e3
Anton <antonzakhlebayeu@gmail.com>
2022-08-14 03:52:05 +0300

    Added auth for redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import * as redisStore from 'cache-manager-redis-store';
       store: redisStore,
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
+      auth_pass: process.env.REDIS_AUTH_PASS,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
